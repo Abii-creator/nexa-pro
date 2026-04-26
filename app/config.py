2ec51bd7ce56e6785c8b27c36ad2ae3791ac8dba
abii-creator <amisda.abii@gmail.com>
2026-04-26 13:35:42 +0300

    fix database for render using tmp

--- app/config.py
+++ app/config.py
@@ -1,16 +1,16 @@
 import os
 
+# Base directory ya project
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-INSTANCE_DIR = os.path.join(BASE_DIR, "..", "instance")
-
-os.makedirs(INSTANCE_DIR, exist_ok=True)
 
 class Config:
+    # Security key (inatoka Render Environment Variables)
     SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
 
+    # Database (Render-friendly path)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///" + os.path.join(INSTANCE_DIR, "event_guest.db")
+        "sqlite:////tmp/event_guest.db"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
