45d16e7840bb6ebe471f682b3758095105f040b5
abii-creator <amisda.abii@gmail.com>
2026-04-26 13:08:22 +0300

    fix database path for render

--- app/config.py
+++ app/config.py
@@ -1,10 +1,16 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+INSTANCE_DIR = os.path.join(BASE_DIR, "..", "instance")
+
+os.makedirs(INSTANCE_DIR, exist_ok=True)
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-        BASE_DIR, "..", "instance", "event_guest.db"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(INSTANCE_DIR, "event_guest.db")
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "super-secret-key"
