5a0c79905e1d9cb9a96e4c81b3832606de742866
abii-creator <amisda.abii@gmail.com>
2026-04-26 13:41:37 +0300

    fix sqlite path for render (4 slashes)

--- app/config.py
+++ app/config.py
@@ -2,8 +2,10 @@ import os
 
 
 class Config:
+    # Secret key kutoka Render environment
     SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
 
+    # Database (IMPORTANT: slash 4 kwa Render)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
         "sqlite:////tmp/event_guest.db"
