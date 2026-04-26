8fd3b1aef942cfdcb53679163f58b19572af62f8
abii-creator <amisda.abii@gmail.com>
2026-04-26 13:39:35 +0300

    fix sqlite path for render

--- app/config.py
+++ app/config.py
@@ -1,13 +1,9 @@
 import os
 
-# Base directory ya project
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    # Security key (inatoka Render Environment Variables)
     SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
 
-    # Database (Render-friendly path)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
         "sqlite:////tmp/event_guest.db"
